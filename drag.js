842ab9dbeb1456b297bc4025d2e7bb96932ce06d
0xSKS <141749855+0xsks@users.noreply.github.com>
2023-09-28 18:43:58 -0600

    Minor update
    CSS revisions
    drag.js smoothed out a little
    starfield animation slower

--- drag.js
+++ drag.js
@@ -1,8 +1,9 @@
 // Get all the move icons
 var moveIcons = document.querySelectorAll('.move-icon');
 
-// Variables to store the dragged card and the initial mouse position
+// Variables to store the dragged card, the placeholder card and the initial mouse position
 var draggedCard = null;
+var placeholderCard = null;
 var initialMousePos = { x: 0, y: 0 };
 
 // Add event listeners for each move icon
@@ -13,6 +14,16 @@ for (var i = 0; i < moveIcons.length; i++) {
   moveIcon.addEventListener('mousedown', function(e) {
     draggedCard = this.parentNode.parentNode; // The card is the grandparent of the move icon
     initialMousePos = { x: e.clientX - draggedCard.offsetLeft, y: e.clientY - draggedCard.offsetTop };
+
+    // Create a placeholder card and set its size to match the dragged card
+    placeholderCard = document.createElement('div');
+    placeholderCard.style.width = `${draggedCard.offsetWidth}px`;
+    placeholderCard.style.height = `${draggedCard.offsetHeight}px`;
+    placeholderCard.style.visibility = 'hidden'; // Make the placeholder card invisible
+
+    // Replace the dragged card with the placeholder card
+    draggedCard.parentNode.insertBefore(placeholderCard, draggedCard);
+
     draggedCard.style.position = 'absolute';
     draggedCard.style.zIndex = '1000';
   });
@@ -23,6 +34,8 @@ document.addEventListener('mouseup', function(e) {
   if (draggedCard) {
     draggedCard.style.zIndex = '';
     draggedCard = null;
+    // Do not remove the placeholder card
+    placeholderCard = null;
   }
 });
 
